3db17447b7b6b4193508864cdbf2e1808eb216b3
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-06-07 23:05:43 +0300

    wrote some test function

--- quiz.js
+++ quiz.js
@@ -10,3 +10,13 @@ show.classList.add('intrebare-2_active');
 show.scrollIntoView({behavior : "smooth"});
 });
 
+// de folosit mai tarziu
+
+// let submit = document.getElementById('submit');
+
+// submit.addEventListener('click', function() {
+//     let goodAnswers = 4;
+//    alert(`You have completed the test with ${goodAnswers} answers!`);
+
+// })
+
