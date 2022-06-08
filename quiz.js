b1fde87bb92882efd7e1f0c2cf18745ff13b3a19
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-06-08 23:37:46 +0300

    wrote the function for validating the answers for Q1, by looping through the answers

--- quiz.js
+++ quiz.js
@@ -10,6 +10,35 @@ show.classList.add('intrebare-2_active');
 show.scrollIntoView({behavior : "smooth"});
 });
 
+let answer1 = document.getElementById('answer1');
+
+const radioButtons = document.querySelectorAll('input[name = "planetei"]');
+
+answer1.addEventListener('click', (event) => {
+    event.preventDefault()
+    let setAnswer;
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            setAnswer = radioButton.value;
+            break;
+        }
+    }
+      if (setAnswer == "Terra") {
+          console.log("This is the correct answer!!!");
+      } else {
+          console.log("No, the correct answer was 'Terra'");
+      }
+
+})
+
+
+
+
+
+
+
+
+
 // de folosit mai tarziu
 
 // let submit = document.getElementById('submit');
